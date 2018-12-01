9a27a5e7c60892969711f4dd7b98fff095d2e299
taiki-saito <neural.neuman921@gmail.com>
2018-12-01 18:03:57 +0900

    add a test request

---
+++ src/TogglClient.ts
@@ -0,0 +1,19 @@
+function testRequest(): void {
+  let url = 'https://www.toggl.com/api/v8/me';
+  let options = {
+    method: 'get',
+    headers: {"Authorization" : " Basic " + Utilities.base64Encode("51135b1325b711d9e4a594c30ed5ce52:api_token")}
+  };
+  let result = UrlFetchApp.fetch(url, options);
+  console.log(result)
+}
+
+class TogglApp {
+  apiToken: string;
+  headers: {Authorization: string;};
+  private const = 'https://api.chatwork.com/v8';
+  constructor(apiToken: string) {
+    this.headers = {Authorization: ' Basic ' + Utilities.base64Encode(apiToken)}
+  }
+
+}
